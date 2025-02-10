69be61efde8beb83fd11c0b25d12d78e3c9359c8
Rodolfo-Cesar <desenhandomerda@gmail.com>
2025-02-10 18:26:33 +0000

    Adicionei uma operação para calcular a Densidade populacional e o PIB per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,14 +7,12 @@ int main() {
 //variáveis declaradas e inicializadas! 
 
 char nomedacidade[50]="nenhum!" ;
-
 int populacao = 0 ;
-
 int numerodepontos = 0 ;
-
 float area = 0.000; 
-
 float PIB = 0.000;
+float densipopulacional = 0.000;
+float PIBpercapita = 0.000;
 
 // sitema de entrada e saída de dados
 
@@ -25,6 +23,7 @@ scanf("%s", &nomedacidade );
 printf("\nQual o número da população da cidade? \n ");
 printf("Sua resposta: ");
 scanf("%d", &populacao );
+
 
 printf("\nQuantos pontos turísticos a cidade tem? \n ");
 printf("Sua resposta: ");
@@ -38,14 +37,18 @@ printf("\nQual o PIB da cidade?  \n");
 printf("Sua resposta: ");
 scanf("%f", &PIB );
 
+//calculo da densidade populacional e PIB per capita
+densipopulacional = populacao / area ;
+PIBpercapita = PIB / populacao;
+
+
 //resultado
 
-printf("\n\nNome da cidade: %s \nPopulação: %d \nNúmero de pontos turísticos: %d \nÁrea da cidade: %f \nPIB: %f \n\n ", nomedacidade, populacao, numerodepontos, area, PIB );
+printf("\n\nNome da cidade: %s \nPopulação: %d \nNúmero de pontos turísticos: %d \nÁrea da cidade: %.2f Km² \nDensidade populacional: %.2f por km² \nPIB: %.2f \nPIB per capita: %.2f \n\n ", nomedacidade, populacao, numerodepontos, area, densipopulacional,  PIB, PIBpercapita );
 
 
-//commit (minha primeira atividade,  não sei como funciona! )
 
-printf("Novo commit");
+
 
 
 
